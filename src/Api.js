98532c9618e320a129257484c817f6943e1b638e
stephen <stephenlancaster1981@hotmail.co.uk>
2023-07-21 16:15:31 +0100

    work on ticket 7

--- src/Api.js
+++ src/Api.js
@@ -23,3 +23,23 @@ export const getComments = (article_id) =>{
         return data.comments;
     })
 }
+
+export const increaseVote = (article_id) => {
+    return axios
+    .patch(`https://stephennc-news.onrender.com/api/articles/${article_id}`, { inc_votes : 1 })
+    .then((response)=> {
+        return response.data;
+    })
+    }
+
+export const decreaseVote = (article_id) => {
+    return axios
+    .patch(`https://stephennc-news.onrender.com/api/articles/${article_id}`, { inc_votes : -1 })
+    .then(response => {
+        return response.data
+    })
+}
+// an object in the form { inc_votes: newVote }.
+// newVote will indicate how much the votes property in the database should be updated by, e.g.
+// { inc_votes : 1 } would increment the current article's vote property by 1
+// { inc_votes : -100 } would decrement the current article's vote property by 100
--- src/SingleArticle.jsx
+++ src/SingleArticle.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
-import { getSingleArticle , getComments} from './Api';
+import { getSingleArticle , getComments, increaseVote, decreaseVote} from './Api';
 import './css/SingleArticle.css'
 import Comments from './Comments';
 
@@ -11,14 +11,19 @@ const SingleArticle = () => {
     const [loading, setLoading] = useState(true)
     const [comments , setComments] =useState(null)
     const [showComments, setShowComments] = useState(false)
+    const [voted, setVoted] = useState(false)
+    const [voteErr, setVoteErr] = useState(false)
+    const [voteCount, setVoteCount] = useState(0)
+    const [initialVoteCount, setInitialVoteCount] = useState(0)
 
     useEffect(() => {
         getSingleArticle(article_id)
         .then((data) => {
             setArticle(data)
+            setInitialVoteCount(data.votes)
             setLoading(false)
         })
-    },[article_id])
+    },[article_id,voted])
 
     if(loading) {
         return <h1>Loading...</h1>
@@ -31,6 +36,43 @@ const SingleArticle = () => {
             setShowComments(true)
         
         })
+}
+
+const optimisticVote = (value) => {
+    if ((value === 1 && !voted) || (value === -1 && voted)) {
+      setVoteCount((prevVoteCount) => prevVoteCount + value);
+      setVoted((prevVoted) => !prevVoted);
+
+      if (value === 1) {
+        increaseVote(article_id).catch(() => {
+          setVoteCount((prevVoteCount) => prevVoteCount - value);
+          setVoted((prevVoted) => !prevVoted);
+        });
+      } else {
+        decreaseVote(article_id).catch(() => {
+          setVoteCount((prevVoteCount) => prevVoteCount - value);
+          setVoted((prevVoted) => !prevVoted);
+
+        });
+      }
+    }
+  };
+  
+const upVote = () => {
+    optimisticVote(1)
+    // increaseVote(article_id)
+    // .then(()=>{
+    //     setVoted(true)
+    //}
+    //)
+}
+
+const downVote = () => {
+    optimisticVote(-1)
+    // decreaseVote(article_id)
+    // .then(()=>{
+    //     setVoted(false)
+    // })
 }
 
 
@@ -46,8 +88,12 @@ const SingleArticle = () => {
     <p className='author'>{author}</p>
     <label style={{ color: 'blue' }} htmlFor='created_at'>Posted:</label>
     <p className='created_at'>{created_at}</p>
-    <label style={{ color: 'blue' }} htmlFor='votes'>Votes:</label>
-    <p className='votes'>{votes}</p>
+    <div>
+        <label style={{ color: 'blue' }} htmlFor='votes'>Votes:</label>
+        <p className='votes'>{initialVoteCount + voteCount}</p>
+        {!voted &&<button onClick={upVote}>Click to Like Article</button>}
+        {voted &&<button onClick={downVote}>Click to unlike Article</button>}
+    </div>
     <Link to="/">Back to Articles</Link>
     <button onClick = {commentsClicked}>Comments</button>
     {showComments && <Comments comments={comments} />}
